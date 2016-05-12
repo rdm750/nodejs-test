6550f8ba40804932a8f7449b162040c2ad82c154
rdm750 <rdm750@gmail.com>
2016-05-12 18:37:16 -0400

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
--var train =1;		
- -while(train<=8){debug("test");train++;}		
- -		
- -confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');		
- -prompt("What is your name?");		
- -///System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc train.js 		
- -//export JSC_HOME=/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/		
- -//export PATH=$PATH:$JSC_HOME		
- -
